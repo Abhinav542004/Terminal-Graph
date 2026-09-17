6fccfbafb581d3731dbfee5a3de76b6218ec840e
Abhinav Kalala <abhinavkalala123@gmail.com>
2026-09-17 17:03:26 +0000

    new generate.h

--- generate.h
+++ generate.h
@@ -1,10 +1,7 @@
 #ifndef GENERATE_H
 #define GENERATE_H
 
-void generate_graph();
 #include <vector>
 #include <fstream>
 void generate_graph(const std::vector<int>& values, std::ofstream& output);
 #endif
-
-//it will take vector int and it will take file pointe where it needs to print bargraph
